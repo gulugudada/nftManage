bc148350bce5680edaffd77b7933799dfc9e46ef
gulugudada <92870394+gulugudada@users.noreply.github.com>
2022-04-04 18:15:14 +0800

    Add files via upload

---
+++ 后端/src/main/java/com/nftmanage/nftmanage/NftManageApplication.java
@@ -0,0 +1,13 @@
+package com.nftmanage.nftmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftManageApplication.class, args);
+    }
+
+}
